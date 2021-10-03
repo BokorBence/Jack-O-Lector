7effbedc952ca9e860dedfdf0588f0d2c1632cc3
Dominik-Bako <SVT6EY@INF.ELTE.hu>
2021-10-03 16:44:40 +0200

    Gameloop started, button appears

--- include/button.hpp
+++ include/button.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "../SDL2/include/SDL.h"
 #include <string>
+#include "../SDL2/include/SDL_surface.h"
 
 class Button
 {   
@@ -9,7 +10,7 @@ class Button
         struct { int r, g, b, a;} colour;
         std::string text;
         void button_proccess_event(const SDL_Event* ev);
-        bool buttonPressed(SDL_Renderer *r);
+        bool draw_button(SDL_Renderer *r);
         Button(int r, int g, int b, int a, int x, int y, int width, int height, std::string text_);
         ~Button();
     private:
