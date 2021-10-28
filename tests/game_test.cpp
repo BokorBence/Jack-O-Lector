8303aea46dddc6d1e6fc1ba612e9330cf5425065
Dominik-Bako <SVT6EY@INF.ELTE.hu>
2021-10-29 00:36:43 +0200

    Guard movement tests added

--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "../include/guard.hpp"
 // uncomment to disable assert()
 // #define NDEBUG
 #include <cassert>
@@ -9,4 +10,16 @@
 int main()
 {
     assert(2+2==4);
+    Guard g = Guard(0, 0, 0, true, 1);
+    assert(g.get_x() == 0);
+    assert(g.get_y() == 0);
+    assert(g.get_d() == 0);
+    g.moveDown();
+    assert(g.get_y() == 1);
+    g.moveUp();
+    assert(g.get_y() == 0);
+    g.moveRight();
+    assert(g.get_x() == 1);
+    g.moveLeft();
+    assert(g.get_x() == 0);
 }
