99c9e2eb94295dcf37f674083ee3ecf736a73653
BokorBence <BokorBence@users.noreply.github.com>
2021-11-16 11:45:50 +0000

    deploy: 66b918ee9aa1e168cf7eee1ff09f57d67154463c

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "bdeghlmsw",
+  0: "bdeghlmnpsw",
   1: "beglmsw",
-  2: "dhm"
+  2: "dhm",
+  3: "np"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "variables"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Functions",
+  3: "Variables"
 };
 
